5188aa594b20437bddd74a8113f78a29749b6e1f
Andres Suarez <andresfelipe9619@gmail.com>
2022-10-08 01:08:02 -0500

    :recycle: Recreate backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,47 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "userPoolGroups": {
+            "DoctorsGroupRole": "string",
+            "PatientsGroupRole": "string"
+        },
+        "telemonitoringCognito": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "function": {
+        "telemonitoringCognitoPostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "telemonitoringAPI": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "api": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "hosting": {
+        "S3AndCloudFront": {
+            "Region": "string",
+            "HostingBucketName": "string",
+            "WebsiteURL": "string",
+            "S3BucketSecureURL": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
-import { useState } from 'react';
-import { API, Amplify } from 'aws-amplify';
-import { withAuthenticator, Button, Heading, Flex } from '@aws-amplify/ui-react';
-import logo from './logo.svg';
-import awsExports from "./aws-exports";
-import './App.css';
-import '@aws-amplify/ui-react/styles.css';
+import { useState } from 'react'
+import { API, Amplify } from 'aws-amplify'
+import { withAuthenticator, Button, Heading, Flex } from '@aws-amplify/ui-react'
+import logo from './logo.svg'
+import awsExports from './aws-exports'
+import './App.css'
+import '@aws-amplify/ui-react/styles.css'
 
-Amplify.configure(awsExports);
+Amplify.configure(awsExports)
 
-function App({ user, signOut }) {
+function App ({ user, signOut }) {
   const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  async function getTelemonitoringData() {
-    const response = await API.get('api76e270a8', '/telemonitoring/timestamp');
-    console.log('response', response)
-    setData(response)
+  async function getTelemonitoringData () {
+    try {
+      setLoading(true)
+      const response = await API.get('api', '/telemonitoring/timestamp')
+      console.log('response', response)
+      setData(response)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
-    <div className="App">
-      <Flex direction={"column"} justifyContent="center" alignContent={"center"} className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <Flex
+        direction={'column'}
+        justifyContent='center'
+        alignContent={'center'}
+        className='App-header'
+      >
+        <img src={logo} className='App-logo' alt='logo' />
         <Heading level={1}>Hello {user.username}</Heading>
-        {!data.length && <Button onClick={getTelemonitoringData}>Get Telemonitoring</Button>}
-        {data.map((item, i) => <Heading level={2}>Index - {i}</Heading>)}
+        {!data.length && (
+          <Button onClick={getTelemonitoringData}>
+            {loading ? 'Loading...' : 'Get Telemonitoring'}
+          </Button>
+        )}
+        {data.map((item, i) => (
+          <Heading level={2}>{i} - {item.device_id}; HB: {item.HeartBeat}; SPO2: {item.Spo2}</Heading>
+        ))}
         <Button onClick={signOut}>Sign out</Button>
       </Flex>
     </div>
-  );
+  )
 }
 
-export default withAuthenticator(App);
+export default withAuthenticator(App)
