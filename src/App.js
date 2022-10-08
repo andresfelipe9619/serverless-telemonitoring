995a44a72348001e58bc039c40aee8e8d200a535
Andres Suarez <andresfelipe9619@gmail.com>
2022-10-07 23:09:36 -0500

    :construction: Connect client to Amplify

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import { useState } from 'react';
+import { API, Amplify } from 'aws-amplify';
+import { withAuthenticator, Button, Heading, Flex } from '@aws-amplify/ui-react';
 import logo from './logo.svg';
+import awsExports from "./aws-exports";
 import './App.css';
 import '@aws-amplify/ui-react/styles.css';
 
+Amplify.configure(awsExports);
+
 function App({ user, signOut }) {
+  const [data, setData] = useState([])
+
+  async function getTelemonitoringData() {
+    const response = await API.get('api76e270a8', '/telemonitoring/timestamp');
+    console.log('response', response)
+    setData(response)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Flex direction={"column"} justifyContent="center" alignContent={"center"} className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Heading level={1}>Hello {user.username}</Heading>
+        {!data.length && <Button onClick={getTelemonitoringData}>Get Telemonitoring</Button>}
+        {data.map((item, i) => <Heading level={2}>Index - {i}</Heading>)}
         <Button onClick={signOut}>Sign out</Button>
-      </header>
+      </Flex>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Amplify } from 'aws-amplify';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import awsExports from "./aws-exports";
 import './index.css';
 
-Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
