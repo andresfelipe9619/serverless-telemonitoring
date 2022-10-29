db2400333ede78de949ecd34499266e13700e246
Andres Suarez <andresfelipe9619@gmail.com>
2022-10-29 12:46:44 -0500

    :bug: Update validation

--- src/pages/auth/settings.js
+++ src/pages/auth/settings.js
@@ -4,10 +4,8 @@ export const validationSchema = Yup.object().shape({
   height: Yup.number().required('Obligatory field'),
   weight: Yup.number().required('Obligatory field'),
   phone: Yup.number().required('Obligatory field'),
-  email: Yup.string()
-    .email()
-    .required('Obligatory field'),
   name: Yup.string().required('Obligatory field'),
+  birthdate: Yup.string().required('Obligatory field'),
   lastname: Yup.string().required('Obligatory field')
 })
 
@@ -15,7 +13,7 @@ export const initialValues = {
   height: 0,
   weight: 0,
   phone: 0,
-  email: '',
+  birthdate: '',
   name: '',
   lastname: ''
 }
