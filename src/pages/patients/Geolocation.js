26e3acf81cc455880e2fde6016d4c9762c5d7e8a
Andres Suarez <andresfelipe9619@gmail.com>
2022-11-06 23:01:51 -0500

    :construction: Get user's location

--- src/pages/patients/Geolocation.js
+++ src/pages/patients/Geolocation.js
@@ -1,12 +1,25 @@
 import { Flex, TextField, View } from '@aws-amplify/ui-react'
 import 'leaflet'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
-const position = [51.505, -0.09]
+export default function Geolocation ({ locateUser = true }) {
+  const [latitude, setLatitude] = useState(null)
+  const [longitude, setLongitude] = useState(null)
 
-export default function Geolocation () {
-  const [coords] = useState(position)
+  const coords = [latitude, longitude]
+
+  useEffect(() => {
+    if (!locateUser) return
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const { latitude, longitude } = position.coords
+
+      setLatitude(latitude)
+      setLongitude(longitude)
+    })
+  }, [locateUser])
+
+  console.log('coords', coords)
 
   return (
     <View width={'100%'}>
@@ -14,30 +27,32 @@ export default function Geolocation () {
         <TextField
           name='latitude'
           label='Latitude'
-          placeholder={position[0]}
+          placeholder={latitude}
           type='number'
         />
         <TextField
           name='longitude'
           label='Longitude'
-          placeholder={position[1]}
+          placeholder={longitude}
           type='number'
         />
       </Flex>
-      <MapContainer
-        center={coords}
-        zoom={13}
-        scrollWheelZoom={false}
-        style={{ height: 200 }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={coords}>
-          <Popup>A pretty CSS3 popup.</Popup>
-        </Marker>
-      </MapContainer>
+      {latitude && longitude && (
+        <MapContainer
+          center={coords}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ height: 300 }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={coords}>
+            <Popup>A pretty CSS3 popup.</Popup>
+          </Marker>
+        </MapContainer>
+      )}
     </View>
   )
 }
