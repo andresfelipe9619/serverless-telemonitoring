906d84d19b149c2e1b53cf9f0f54a1d48816033f
Andres Suarez <andresfelipe9619@gmail.com>
2022-12-15 19:27:21 -0500

    :children-crossing: Improve texts

--- amplify/backend/function/cronlambda/src/utils.js
+++ amplify/backend/function/cronlambda/src/utils.js
@@ -1,65 +1,51 @@
 const getHeartbeatIndicator = average => {
-  if (average >= 98) {
+  if (average >= 100) {
     return {
-      name: 'Inadecuado',
+      name: 'Taquicardia',
       color: 'var(--amplify-colors-red-40)',
-      tips: ''
+      tips: 'Es necesario contactar a su médico tratante para recibir atención prioritaria, Evitar la ingesta de estimulantes, mantener una adecuada idratación y realizar ejercicios de respiración pueden ayudar.'
     }
   }
-  if (average >= 80) {
+  if (average >= 60) {
     return {
       name: 'Normal',
-      color: 'var(--amplify-colors-orange-40)',
-      tips: ''
-    }
-  }
-  if (average >= 70) {
-    return {
-      name: 'Bueno',
-      color: 'var(--amplify-colors-yellow-40)',
-      tips: ''
-    }
-  }
-  if (average >= 40) {
-    return {
-      name: 'Excelente',
       color: 'var(--amplify-colors-green-40)',
       tips: ''
     }
   }
   return {
-    name: 'Inadecuado',
+    name: 'Bradicardia',
     color: 'var(--amplify-colors-red-40)',
-    tips: ''
+    tips: 'Es necesario contactar a su médico tratante para recibir atención proritaria, Hacer actividad la mayoría de los días de la semana o bien todos los días. Su médico puede indicarle qué nivel de ejercicio es su adecuado.'
   }
 }
 
 const getSpo2Indicator = average => {
-  if (average >= 98) {
+  if (average >= 95) {
     return {
       name: 'Normal',
       color: 'var(--amplify-colors-green-40)',
-      tips: 'Todo bien'
+      tips: 'Realizar ejercicio de manera regular, llevar una dieta rica en hierro y tomar aire fresco puede ayudar con tu salud.'
     }
   }
-  if (average >= 95) {
+  if (average >= 91) {
     return {
-      name: 'Insuficiente',
+      name: 'Hipoxia Leve',
       color: 'var(--amplify-colors-yellow-40)',
-      tips: 'Tolerable, paciente dificlmente nota influencia alguna'
+      tips: 'Es necesario contactar a su médico tratante para identificar los síntomas que indiquen esta disminución.'
     }
   }
-  if (average >= 90) {
+  if (average >= 86) {
     return {
-      name: 'Disminuido',
+      name: 'Hipoxia Moderada',
       color: 'var(--amplify-colors-orange-40)',
-      tips: 'Intervención inmediata'
+      tips: 'Es necesario contactar a su médico lo antes posible para identificar los síntomas que indiquen esta disminución, debe adquirir suministro de oxigeno asistido'
     }
   }
   return {
-    name: 'Crítico',
+    name: 'Hipoxia Grave',
     color: 'var(--amplify-colors-red-40)',
-    tips: 'Remitir a un especialista'
+    tips: 'Es necesario contactar a su médico lo antes posible para identificar los síntomas que indiquen esta disminución, debe adquirir suministro de oxigeno asistido de manera inmediata.'
   }
 }
 
