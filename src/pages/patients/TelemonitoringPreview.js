f30ae845cabdaa3d4fb88df4bf854ce33ab6b304
Andres Suarez <andresfelipe9619@gmail.com>
2022-12-18 20:35:39 -0500

    :white-check-mark: Allow to test telemonitoring

--- src/pages/patients/TelemonitoringPreview.js
+++ src/pages/patients/TelemonitoringPreview.js
@@ -1,24 +1,35 @@
 import { Loader, View } from '@aws-amplify/ui-react'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import useTelemonitoring from '../../hooks/useTelemonitoring'
 import ErrorAlert from '../error/ErrorAlert'
 import Chart from '../reports/Chart'
 import sub from 'date-fns/sub'
+import set from 'date-fns/set'
 
 const DELAY = 2000
 const SECONDS = DELAY / 1000
+const TESTING = false
 
 export default function TelemonitoringPreview ({ device }) {
   const [{ data, error, loading }, { getTelemonitoringData }] =
     useTelemonitoring()
   const lastDevice = usePrevious(device)
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
     if (!device) return
 
     function tick () {
       const concat = lastDevice === device
-      const today = new Date()
+      let today = new Date()
+      if (TESTING) {
+        today = set(new Date('2022-12-16'), {
+          hours: 9,
+          minutes: 51,
+          seconds: count * SECONDS,
+          milliseconds: 0
+        })
+      }
       const start = sub(today, { seconds: SECONDS })
       const startDate = start.toISOString()
       const endDate = today.toISOString()
@@ -28,13 +39,19 @@ export default function TelemonitoringPreview ({ device }) {
         startDate,
         endDate
       }
-
+      if (TESTING) setCount(prev => ++prev)
       getTelemonitoringData(device, filters)
     }
-    let id = setInterval(tick, DELAY)
-    return () => clearInterval(id)
+
+    if (!count && TESTING) {
+      tick()
+      setCount(1)
+    } else {
+      let id = setInterval(tick, DELAY)
+      return () => clearInterval(id)
+    }
     // eslint-disable-next-line
-  }, [device])
+  }, [device, count])
 
   return (
     <View height='60vh'>
